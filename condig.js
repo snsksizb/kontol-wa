adf2cc98c3e4839f340eb6ed591342e09ab21626
snsksizb <kkonyol178@gmail.com>
2025-08-10 21:14:24 +0700

    Add files via upload

---
+++ condig.js
@@ -0,0 +1,31 @@
+// config.js
+global.owner = ['6283110650507'] // Ganti nomor kamu
+global.mods = ['628xxx'] 
+global.prems = ['628xxx']
+global.packname = 'Danzy kece'
+global.author = 'Termux Bot'
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+
+// Pairing Code Setup
+global.pairingCode = true
+global.phoneNumber = "628xxxxxxxx" // Nomor WhatsApp kamu dengan kode negara
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update ${__filename}`))
+    delete require.cache[file]
+    require(file)
+})
